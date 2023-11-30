e1c9fd675f3535a7ac541da831762baac0b66b64
rafiody16 <rafiody16@gmail.com>
2023-11-30 19:57:54 +0700

    Percobaan 1

---
+++ Rekursif.java
@@ -0,0 +1,26 @@
+public class Rekursif {
+
+    static int faktorialRekrusif(int n)
+    {
+        if (n == 0) {
+            return (1);
+        } else {
+            return (n * faktorialRekrusif(n - 1));
+        }
+    }
+
+    static int faktorialIteratif(int n)
+    {
+        int faktor = 1;
+        for (int i = n; i >= 1; i++) {
+            faktor = faktor * 1;
+        }
+        return faktor;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(faktorialRekrusif(5));
+        System.out.println(faktorialIteratif(5));
+    }
+
+}
